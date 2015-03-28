f2bca06e67571fa4e1d121e7898ca0f28d8333d2
gary2546 <song.ch@husky.neu.edu>
2015-03-28 19:45:33 -0400

    gets correct current url

--- background.js
+++ background.js
@@ -5,15 +5,20 @@ var queue = {
   "index": 0
 };
 
+var url = "https://soundcloud.com";
+
 // Listens to messages from content script and popup script
 chrome.runtime.onMessage.addListener(
   function(message, sender, sendResponse) {
-    console.log(sender);
-    console.log(message);
 
     // if given a track from content script, adds track to the queue
-    if(sender.tab && ("track" in message)) {
-      queue.tracks.push(message.track);
+    if(sender.tab && message.track === "CURRENT_URL") {
+      chrome.tabs.getSelected(null,function(tab) {
+        queue.tracks.push(tab.url);
+      });
+    }
+    else if(sender.tab && ("track" in message)) {
+      queue.tracks.push(url + message.track);
     }
     // if signaled popup is open, send back queue object
     else if(!sender.tab && message.visible) {
