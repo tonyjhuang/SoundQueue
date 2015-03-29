34343f39ce5aaa0a2953555642b2d341a41da81b
Gasper Chan <gchan@nat-wireless-guest-reg-153-50.bu.edu>
2015-03-28 23:37:45 -0400

    fix merge conflicts

--- popup.js
+++ popup.js
@@ -6,7 +6,6 @@ var paused = false;
 
 // highlightes the current played song
 function _highlightSong(index) {
-	console.log($(".song:nth-child(" + index + ")"));
 	$(".song:nth-child(" + index + ")").addClass("highlight");
 }
 
@@ -15,8 +14,21 @@ function _appendToQueue(result) {
 	var title = result.title;
 	var artwork_url = result.artwork_url;
 	var username = result.user.username
-	var html = "<div id='track" + i + "' class='song'><img class='artwork' src='" + artwork_url + "'><p>" + username + "</p><p>" + title + "</p><img src='assets/images/play.svg'></div>";
+	var html = "<div id='track" + i + "' class='song'><img class='artwork' src='" + artwork_url + "'><p>" + username + "</p><p>" + title + "</p></div>";
 	$(".queue-container").append(html);
+}
+
+function _jumpToSong(index) {
+	chrome.runtime.sendMessage({index: index});
+	currentIndex = index;
+}
+
+function _pause(paused) {
+	chrome.runtime.sendMessage({pause: paused});
+}
+
+function _play(playing) {
+	chrome.runtime.sendMessage({play: playing});
 }
 
 // Lets background script know that popup is opened
