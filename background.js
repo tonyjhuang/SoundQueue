574ed874692b19fd47555bd7fc63269b0a54a808
gary2546 <song.ch@husky.neu.edu>
2015-03-28 21:27:41 -0400

    fixed merge error

--- background.js
+++ background.js
@@ -1,18 +1,29 @@
 // keeps track of all the tracks in the queue
 // and the current song playing
 var queue = {
+  /*
+  tracks[0].title for song title
+  tracks[0].url for song url
+  */
   "tracks": [],
   "index": 0
 };
 
 var url = "https://soundcloud.com";
 
+// Uses soundcloud sdk to get song info from an url
 function _resolve(url) {
-  $.get('http://api.soundcloud.com/resolve.json?url=' + url + '&client_id=be1435461b3275ac389c9f47f61e2560',
+  SC.get('http://api.soundcloud.com/resolve.json?url=' + url,
     function(result) {
       queue.tracks.push(result);
-  });
+    }
+  );
 }
+
+// Initializes soundcloud sdk
+SC.initialize({
+  client_id: "be1435461b3275ac389c9f47f61e2560"
+});
 
 // Listens to messages from content script and popup script
 chrome.runtime.onMessage.addListener(
@@ -20,7 +31,7 @@ chrome.runtime.onMessage.addListener(
 
     // if given a track from content script, adds track to the queue
     if(sender.tab && message.track === "CURRENT_URL") {
-      chrome.tabs.getSelected(null,function(tab) {
+      chrome.tabs.getSelected(null, function(tab) {
         _resolve(tab.url);
       });
     }
--- popup.js
+++ popup.js
@@ -1,35 +1,17 @@
-function appendToQueue(result) {
+// add song title to popup queue
+function _appendToQueue(result) {
 	var title = result.title;
 	var html = "<div class='song'><p>" + title + "</p></div>";
 	$(".queue-container").append(title);
 }
 
-function resolve(url) {
-	$.get('http://api.soundcloud.com/resolve.json?url=' + url + '&client_id=be1435461b3275ac389c9f47f61e2560',
-		function(result) {
-			console.log(result);
-			appendToQueue(result);
-	});
-}
-
-function main() {
-	SC.initialize({
-		client_id: "be1435461b3275ac389c9f47f61e2560"
-	});
-
-	// Lets background script know that popup is opened
-	// and gets the queue object as a response
-
-	chrome.runtime.sendMessage({visible:true},
-		function(response) {
-			console.log(response);
-			for (i = 0; i < response.tracks.length; i++) {
-				resolve("https://soundcloud.com" + response.tracks[i]);
-			}
+// Lets background script know that popup is opened
+// and gets the queue object as a response
+chrome.runtime.sendMessage({visible:true},
+	function(response) {
+		tracks = response.tracks;
+		for (i = 0; i < tracks.length; i++) {
+			_appendToQueue(tracks[i]);
 		}
-	);
-
-	
-}
-
-main();
+	}
+);
